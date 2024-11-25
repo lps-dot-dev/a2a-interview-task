e870c0817faf37e5eea116402317af326cc5ded0
Luis Pedroza-Soto <luis.g.pedrozasoto@gmail.com>
2024-11-24 17:37:53 -0800

    Create `theater` table and Eloquent model

---
+++ php/app/Models/Theater.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property int id
+ * @property string name
+ * @property string address
+ * @property DateTimeInterface created_at
+ * @property DateTimeInterface updated_at
+ */
+class Theater extends Model
+{
+    //
+}
