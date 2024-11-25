3049de10d5b64ba636c92508411239c103bd3f53
Luis Pedroza-Soto <luis.g.pedrozasoto@gmail.com>
2024-11-25 13:48:30 -0800

    Add clarity to variable names and setup DB connection

--- go/main.go
+++ go/main.go
@@ -1,18 +1,26 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"os"
 
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/joho/godotenv"
 	"github.com/urfave/cli/v2"
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
+	envError := godotenv.Load()
+	if envError != nil {
 		log.Fatal("Error loading .env file")
+	}
+
+	dataSourceName := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", os.Getenv("DB_USERNAME"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_DATABASE"))
+	databaseConnection, databaseError := sql.Open("mysql", dataSourceName)
+	if databaseError != nil {
+		log.Fatal(databaseError.Error())
 	}
 
 	app := &cli.App{
@@ -34,7 +42,11 @@ func main() {
 		},
 	}
 
-	if err := app.Run(os.Args); err != nil {
-		log.Fatal(err)
+	if appError := app.Run(os.Args); appError != nil {
+		log.Fatal(appError.Error())
 	}
+
+	// defer the close till after the main function has finished
+	// executing
+	defer databaseConnection.Close()
 }
