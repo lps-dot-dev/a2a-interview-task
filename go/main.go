92db872e3a55417bb241f9720d08f3800c5801c5
Luis Pedroza-Soto <luis.g.pedrozasoto@gmail.com>
2024-11-25 13:01:27 -0800

    Add `urfave/cli` package to provide a more expressive way to write apps
    As well as providing easier access to flags and input arguments.

--- go/main.go
+++ go/main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/urfave/cli/v2"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	app := &cli.App{
+		Commands: []*cli.Command{
+			{
+				Name:  "theaters",
+				Usage: "options for theaters.",
+				Subcommands: []*cli.Command{
+					{
+						Name:  "most-sales-by-day",
+						Usage: "find which movie theater generated the most sales for that day.",
+						Action: func(cCtx *cli.Context) error {
+							fmt.Println("Not yet implemented!")
+							return nil
+						},
+					},
+				},
+			},
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
 }
