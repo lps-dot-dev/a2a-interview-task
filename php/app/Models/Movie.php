88de33f054274769c61bc2a7261d1d79773390db
Luis Pedroza-Soto <luis.g.pedrozasoto@gmail.com>
2024-11-24 17:56:40 -0800

    Create `movies` table and Eloquent model

---
+++ php/app/Models/Movie.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * @property int id
+ * @property string name
+ * @property string genre
+ * @property string plot
+ * @property string language
+ * @property string country
+ * @property string rating
+ * @property DateTimeInterface release_date
+ * @property int runtime
+ * @property string poster
+ * @property DateTimeInterface created_at
+ * @property DateTimeInterface updated_at
+ */
+class Movie extends Model
+{
+    //
+}
---
+++ php/database/migrations/2024_11_25_013834_create_movies_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('genre'); // Could also possibly be a separate table for genres
+            $table->text('plot');
+            $table->string('language');
+            $table->string('country');
+            $table->string('rating'); // This can be `rating_id` for a ratings table, but seems excessive right now.
+            $table->timestamp('release_date');
+            $table->integer('runtime');
+            $table->text('poster');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('movies');
+    }
+};
