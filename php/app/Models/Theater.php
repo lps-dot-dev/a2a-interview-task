243a215b409c6869ab6e7142cb8997711d675d86
Luis Pedroza-Soto <luis.g.pedrozasoto@gmail.com>
2024-11-24 19:06:27 -0800

    Create transactions table with Eloquent model
    Also creating an Eloquent model for the join table between movies and theaters so I can leverage the has through relationship to simplify the query logic down the line.

--- php/app/Models/Theater.php
+++ php/app/Models/Theater.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 
 /**
  * @property int id
@@ -17,5 +18,10 @@ class Theater extends Model
     public function movies(): BelongsToMany
     {
         return $this->belongsToMany(Movie::class);
+    }
+
+    public function transactions(): HasManyThrough
+    {
+        return $this->hasManyThrough(TheaterMovieTransaction::class, TheaterMovie::class);
     }
 }
